7b2858b7d54471ffc33b8aef02cc7894b770dc96
thaysa helena <justthaysa@gmail.com>
2022-10-17 20:27:14 -0300

    Trabalhando com testes manuais

---
+++ aula1/index.php
@@ -0,0 +1,19 @@
+<?php
+ 
+ class Slug{
+    private $texto;
+    public function _construct($texto){
+    $this->texto =$texto;
+}
+    
+    public function converter($texto){
+        $resultado = str_replace(" ","-",$this->$texto); // o str_replace irá substituir um determinado valor que está sendo procurado (1 parametro).por outro (2 parametro) em uma determinada string (3 parametro)
+        return strtolower($resultado);
+    }
+
+
+
+}
+
+$meuSlug = new Slug("Teste de backend USANDO PHP");
+echo $meuSlug->converter();
