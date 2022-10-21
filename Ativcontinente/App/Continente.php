ed09d0bfd6edce2bea724d1b860ea95d11a7e64c
thaysa helena <justthaysa@gmail.com>
2022-10-21 20:59:23 -0300

    Finalizado testes_backend

---
+++ Ativcontinente/App/Continente.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+class Continente{
+    private $nome;
+    private $clima;
+    private $idioma;
+    private $pais;
+    
+    public function __construct($nome= "Europa", $clima= "Frio", $idioma= "Espanhol", $pais="Espanha"){
+        $this->nome = $nome;
+        $this->clima = $clima;
+        $this->idioma = $idioma;
+    }  
+
+    public function addPaises(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
